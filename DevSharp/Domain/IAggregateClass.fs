e0e232dde83d421c668168b6d196335aa4e3605c
isierra <isierra@jgingenieros.es>
2016-05-27 09:48:15 +0200

    Organized DevSharp Domain namespace

--- DevSharp/Domain/IAggregateClass.fs
+++ DevSharp/Domain/IAggregateClass.fs
@@ -3,7 +3,6 @@
 open FSharp.Core
 open DevSharp.Validations
 
-
 type CommandType = obj
 type EventType   = obj
 type StateType   = obj
@@ -18,18 +17,3 @@ type IAggregateClass =
     abstract member act: CommandType -> StateType -> EventType seq
 
     abstract member apply: EventType -> StateType -> StateType
-
-
-type NopAggregateClass() =
-
-    interface IAggregateClass with
-
-        member this.validate command = validationResult []
-
-        member this.init = null
-
-        member this.act command state = Seq.empty
-
-        member this.apply event state = null
-
-    
---
+++ DevSharp/Domain/NopAggregateClass.fs
@@ -0,0 +1,17 @@
+﻿namespace DevSharp.Domain
+
+open DevSharp.Validations
+
+type NopAggregateClass() =
+
+    interface IAggregateClass with
+
+        member this.validate command = validationResult []
+
+        member this.init = null
+
+        member this.act command state = Seq.empty
+
+        member this.apply event state = null
+
+    
